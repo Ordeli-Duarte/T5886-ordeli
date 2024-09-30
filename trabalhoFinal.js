a79f476a55557119e70a1a017159b1543a6e9848
Ordeli-Duarte <ordeliduartefilho@gmail.com>
2024-09-30 15:37:17 -0300

    Update trabalhoFinal.js

--- trabalhoFinal.js
+++ trabalhoFinal.js
@@ -7,6 +7,8 @@ let idade
 let peso
 let tipoSanguineo
 let dataUltimaDoacao
+let opcao
+let sistema = true
 
 function menu() {
    console.log("===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====")
@@ -17,44 +19,44 @@ function menu() {
    console.log("4. Buscar doador por data da última doação")
    console.log("5. Sair")
    console.log("_____________________________________________________")
-   return ask.question("Escolha uma opcao: ")
+   opcao = ask.question("Escolha uma opcao: ")
 }
 
-menu()
-
-
-
+while (sistema){
+    menu()
+    switch (opcao) {
+        case '1':
+            cadastrarDoador()
+            break;
+        case '2':
+            listarDoadores()
+            break;
+        case '3':
+            buscarPorTipoSanguineo()
+            break;
+        case '4':
+            buscarPorData()
+            break;
+        case '5':
+            console.clear()
+            console.log("Programa encerrado. ")
+            sistema = false
+            break;
+        default:
+            console.log("Opção inválida. Tente novamente.");
+    }
+}
 
 function cadastrarDoador() {
     nome = ask.question("Digite o nome do doador: ");
     idade = ask.questionInt("Digite a idade do doador: ");
     peso = ask.questionFloat("Digite o peso do doador (kg): ");
-    tipoSanguineo = ask.question("Digite o tipo sanguíneo: ");
-    dataUltimaDoacao = ask.question("Digite a data da última doação (dd/mm/aaaa): ");
-    
+    tipoSanguineo = ask.question("Digite o tipo sanguineo: ");
+    dataUltimaDoacao = ask.question("Digite a data da ultima doacao (dd/mm/aaaa): ");
+    console.clear()
     doadores.push({ nome, idade, peso, tipoSanguineo, dataUltimaDoacao });
     console.log("Doador cadastrado com sucesso!");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
